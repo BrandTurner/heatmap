26bfee6913304b91614229e6ede628dcd25ffe30
John Saleigh <john.saleigh@gmail.com>
2014-04-06 16:15:19 -0700

    more sample sets, shading based on percentages

--- src/costa-sansa.js
+++ src/costa-sansa.js
@@ -8,20 +8,70 @@
     };
 
     $.mockJSON.random = false;
+    $.mockJSON.data['STATE_NAME'] = [
+        "Alabama",
+        "Alaska",
+        "Arizona",
+        "Arkansas",
+        "California",
+        "Colorado",
+        "Connecticut",
+        "Delaware",
+        "Florida",
+        "Georgia",
+        "Hawaii",
+        "Idaho",
+        "Illinois",
+        "Indiana",
+        "Iowa",
+        "Kansas",
+        "Kentucky",
+        "Louisiana",
+        "Maine",
+        "Maryland",
+        "Massachusetts",
+        "Michigan",
+        "Minnesota",
+        "Mississippi",
+        "Missouri",
+        "Montana",
+        "Nebraska",
+        "Nevada",
+        "New Hampshire",
+        "New Jersey",
+        "New Mexico",
+        "New York",
+        "North Carolina",
+        "North Dakota",
+        "Ohio",
+        "Oklahoma",
+        "Oregon",
+        "Pennsylvania",
+        "Rhode Island",
+        "South Carolina",
+        "South Dakota",
+        "Tennessee",
+        "Texas",
+        "Utah",
+        "Vermont",
+        "Virginia",
+        "Washington",
+        "West Virginia",
+        "Wisconsin",
+        "Wyoming"
+    ];
 
     (function(){
         function buildMock(name) {
             $.mockjax({
                 url: '/Contact/List/' + name,
-                responseTime: 750,
+                responseTime: 100,
                 responseText: $.mockJSON.generateFromTemplate({
                     "contacts|10-10": [{
-                        "married|0-1": true,
-                        "email" : "@EMAIL",
-                        "firstName": "@MALE_FIRST_NAME",
-                        "lastName": "@LAST_NAME",
-                        "birthday": "@DATE_MM/@DATE_DD/@DATE_YYYY",
-                        "percentHealth|0-100": 0 
+                        "active|0-1": true,
+                        "stateName": "@STATE_NAME",
+                        "addedDate": "@DATE_MM/@DATE_DD/@DATE_YYYY",
+                        "percentVoted|0-100": 0 
                     }]
                 })
             });
@@ -30,17 +80,18 @@
         buildMock('Alpha');
         buildMock('Bravo');
         buildMock('Charlie');
+        buildMock('Delta');
+        buildMock('Echo');
+        buildMock('Foxtrot');
 
     }());
 
     var grid,
         columns = [
-            {id:"firstName", name:"First Name", field:"firstName"},
-            {id:"lastName", name:"Last Name", field:"lastName"},
-            {id:"email", name:"Email", field:"email"},
-            {id:"percentHealth", name:"% Health", field:"percentHealth", formatter:GraphicalPercentCompleteCellFormatter},
-            {id:"birthday", name:"Birthday", field:"birthday"},
-            {id:"married", name:"Married", field:"married", formatter:BoolCellFormatter}
+            {id:"stateName", name:"First Name", field:"stateName"},
+            {id:"percentVoted", name:"% Voted", field:"percentVoted", formatter:GraphicalPercentCompleteCellFormatter},
+            {id:"addedDate", name:"Date", field:"addedDate"},
+            {id:"active", name:"Active", field:"active", formatter:BoolCellFormatter}
         ],
         options = {
             editable: true,
@@ -48,15 +99,48 @@
             enableCellNavigation: true,
             enableCellReorder: true,
             rowCssClasses: function(item) {
-                return (item.percentHealth >= 80) ?  "healthy" : "";
+                return (item.percentVoted >= 80) ?  "healthy" : "";
             }
         };
 
+    function fillColorForContact(contact) {
+        var goal = {
+            red: 10,
+            green: 15,
+            blue: 200
+        };
+
+        function channel (top,bottom,percent) {
+            return (((top - bottom) * percent) + bottom) >>> 0;
+        }
+
+        var percent = contact.percentVoted / 100.0;
+        var red = channel(goal.red, 255, percent);
+        var green = channel(goal.green, 255, percent);
+        var blue = channel(goal.blue, 255, percent);
+
+        var channelRed = ("00" + red.toString(16)).slice(-2);
+        var channelGreen = ("00" + green.toString(16)).slice(-2);
+        var channelBlue = ("00" + blue.toString(16)).slice(-2);
+
+        var fill = "#"+channelRed+channelGreen+channelBlue;
+
+        console.log('color: (%o, %o, %o) - %o',red,green,blue,fill);
+
+        return fill;
+    }
+
     function renderContacts(contacts, listName) {
         grid = new Slick.Grid("#myGrid",  contacts, columns, options);
-        var svg = $('#data-map').svg('get');
-        var g = svg.getElementById(listName)
-        $(g).children().attr('fill','red');
+        var svg = $('#data-map').svg('get'),
+            g, i, contact, fill;
+
+        for(i = 0; i < 10; i++) {
+            g = svg.getElementById('Layer_' + (i+1));
+            contact = contacts[i];
+            fill = fillColorForContact(contact) ;
+            $(g).children().attr('fill',fill);
+        }
     }
 
     function loadList(name) {        
